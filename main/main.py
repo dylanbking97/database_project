62d11fac8be66f174211ea23fce2822073cac606
justincong <justincong324@gmail.com>
2018-12-09 12:55:00 -0800

    added DB boilerplate

--- main/main.py
+++ main/main.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.heroku import Heroku
 
+
+# boilerplate code
 app = Flask(__name__)
+heroku = Heroku(app)
+
+# configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 
 @app.route('/')
