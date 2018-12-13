652f4d4b81988cb8543484caeb338d788155e8ee
justincong <justincong324@gmail.com>
2018-12-12 20:23:05 -0800

    added dict and list of team names for user query

--- views.py
+++ views.py
@@ -3,6 +3,46 @@ from models import db, Teams
 from flask import render_template, request
 from helpers import *
 
+# dict mapping the long names to the abbreviations
+teams = {"Atlanta Hawks": "ATL",
+        "Boston Celtics": "BOS",
+        "Brooklyn Nets": "BRK",
+        "Charlotte Hornets": "CHO",
+        "Chicago Bulls": "CHI",
+        "Cleveland Cavaliers": "CLE",
+        "Dallas Mavericks": "DAL",
+        "Denver Nuggets": "DEN",
+        "Detroit Pistons": "DET",
+        "Denver Nuggets": "DEN",
+        "Golden State Warriors": "GSW",
+        "Houston Rockets": "HOU",
+        "Indiana Pacers": "IND",
+        "Los Angeles Clippers": "LAC",
+        "Los Angeles Lakers": "LAL",
+        "Memphis Grizzlies": "MEM",
+        "Miami Heat": "MIA",
+        "Milwaukee Bucks": "MIL",
+        "Minnesota Timberwolves": "MIN",
+        "New Orleans Pelicans": "NOP",
+        "New York Knicks": "NYK",
+        "Oklahoma City Thunder": "OKC",
+        "Orlando Magic": "ORL",
+        "Philadelphia 76ers": "PHI",
+        "Phoenix Suns": "PHO",
+        "Portland Trailblazers": "POR",
+        "Sacramento Kings": "SAC",
+        "San Antonio Spurs": "SAS",
+        "Toronto Raptors": "TOR",
+        "Utah Jazz": "UTA",
+        "Washington Wizards": "WAS"
+        }
+
+long_names = []
+
+# loop through teams and place the long names in the list
+for key in teams:
+    long_names.append(key)
+
 # Base page
 @app.route('/')
 def index():
